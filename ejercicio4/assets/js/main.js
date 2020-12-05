8cefabb804c8e8b4f91cda1abae374b20540fafd
JuanDuran85 <duran85juan@gmail.com>
2020-12-05 13:13:21 -0300

    modificando componentes

--- ejercicio4/assets/js/main.js
+++ ejercicio4/assets/js/main.js
@@ -1,25 +1,50 @@
-Vue.component('plan-picker', {
-    template: '#plan-picker-template',
-    data() {
-        return {
-            plans: ['The Single','The Curious','The Addict']
-        }
-    },
-});
-
-Vue.component('plan', {
-    template: '#plan-template',
+let planPickerItemComponent = {
+    template: '#plan-picker-item-template',
     props: {
         name: {
             type: String,
             required: true,
             default: "The plans...",
         }
-    }
-});
+    },
+    methods: {
+        isSelected(){
+            this.$emit('select', this.name);
+        }
+    },
+};
+
+let planPickerComponent = {
+    template: '#plan-picker-template',
+    components: {
+        planPickerItemComponent,
+    },
+    data() {
+        return {
+            plans: [
+                {name: 'The Single', selected: false},
+                {name: 'The Curious', selected: false},
+                {name: 'The Addict', selected: false},
+            ],
+        }
+    },
+    methods: {
+        isSelectedPlan(planName){
+            let changeSelected = this.plans.filter(result => result.name != planName);
+            for (const key in changeSelected) {
+                changeSelected[key].selected = false;
+            }
+            let selectedPlan = this.plans.find(result => result.name == planName);
+            selectedPlan.selected = !selectedPlan.selected;
+        }
+    },
+};
 
 const vm = new Vue({
     el: '#app',
+    components: {
+        planPickerComponent
+    }
 });
 
 

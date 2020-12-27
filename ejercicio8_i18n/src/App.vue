5afbdfe01404ee17199b35c48cce26d0ab36016d
JuanDuran85 <duran85juan@gmail.com>
2020-12-27 19:50:24 -0300

    usando vue i18n

---
+++ ejercicio8_i18n/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Portada />
+  </div>
+</template>
+
+<script>
+import Portada from './components/Portada.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Portada
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ejercicio8_i18n/src/components/Portada.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="hello">
+    <h1>Usando vue-i18n</h1>
+    <h2>{{localised('hola')}}</h2>
+    <h2>{{localised('adios')}}</h2>
+    <h2>{{localised('agua')}}</h2>
+    <button @click="activeLocale='es'">Castellano</button>
+    <button @click="activeLocale='ca'">Català</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Portada',
+  data() {
+    return {
+      activeLocale: 'es',
+      mensaje: {
+        es: {
+          hola: 'Hola',
+          adios: 'Adiós',
+          agua: 'Agua'
+        },
+        ca: {
+          hola: 'Hola',
+          adios: 'Adéu',
+          agua: 'aigua'
+        }
+      }
+    }
+  },
+  methods: {
+    localised(key){
+      return this.mensaje[this.activeLocale][key];
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
